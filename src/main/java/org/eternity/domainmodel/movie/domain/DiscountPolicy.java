66c81a12a505659a5960df920caf9e5a46bacbca
조영호 <aeternum9342@gmail.com>
2025-01-22 01:36:30 +0900

    프로젝트 초기화

---
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -0,0 +1,26 @@
+package org.eternity.domainmodel.movie.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="policy_type")
+@NoArgsConstructor @Getter
+public abstract class DiscountPolicy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name="POLICY_ID")
+    @org.hibernate.annotations.BatchSize(size = 2)
+    private Set<DiscountCondition> conditions = new HashSet<>();
+
+    public DiscountPolicy(Set<DiscountCondition> conditions) {
+        this.conditions = conditions;
+    }
+}
