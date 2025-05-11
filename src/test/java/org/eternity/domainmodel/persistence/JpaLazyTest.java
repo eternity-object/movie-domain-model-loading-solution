245f01c0e5bcd899ee1aeaffd48ab91f79d31d5c
조영호 <aeternum9342@gmail.com>
2025-05-11 23:58:53 +0900

    fetch join 테스트 케이스 추가

--- src/test/java/org/eternity/domainmodel/persistence/JpaLazyTest.java
+++ src/test/java/org/eternity/domainmodel/persistence/JpaLazyTest.java
@@ -10,13 +10,37 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest(showSql = false)
 public class JpaLazyTest {
 	@Autowired
 	private EntityManager em;
+
+	@Test
+	public void fetch_join() {
+		DiscountPolicy policy =
+				new AmountDiscountPolicy(Money.wons(1000),
+						Set.of(
+								new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(9,0), LocalTime.of(11, 0)),
+								new SequenceCondition(1),
+								new SequenceCondition(3)));
+
+		em.persist(policy);
+		em.flush();
+		em.clear();
+
+
+		List<DiscountPolicy> policies =
+				em.createQuery("select p from DiscountPolicy p join fetch p.conditions", DiscountPolicy.class)
+						.getResultList();
+
+		assertThat(policies.size()).isEqualTo(1);
+	}
 
 	@Test
 	public void discount_policy_fetch() {
